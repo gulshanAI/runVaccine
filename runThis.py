ef73630220803ce5f2f745d78cdeedf78f851093
gulshanai <gulshanprajapati5105@gmail.com>
2021-06-24 17:36:32 +0530

    updated

--- runThis.py
+++ runThis.py
@@ -1,13 +1,13 @@
 import requests
 import time
 
-# while(True):
-#     response = requests.get('https://vaccinetouchmedia.herokuapp.com/')
-#     print("Called")
-#     print(response.status_code)
-#     print("Waiting for 30 seconds...")
-#     time.sleep(30)
+while(True):
+    response = requests.get('https://vaccinetouchmedia.herokuapp.com/')
+    print("Called")
+    print(response.status_code)
+    print("Waiting for 30 seconds...")
+    time.sleep(30)
 
 
-response = requests.get('https://vaccinetouchmedia.herokuapp.com/')
-print(response.status_code)
+# response = requests.get('https://vaccinetouchmedia.herokuapp.com/')
+# print(response.status_code)
