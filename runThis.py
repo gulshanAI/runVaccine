d69d2060b82a7f669c58982a044cd82c4861a10e
gulshanai <gulshanprajapati5105@gmail.com>
2021-06-24 17:30:21 +0530

    first commit

---
+++ runThis.py
@@ -0,0 +1,13 @@
+import requests
+import time
+
+# while(True):
+#     response = requests.get('https://vaccinetouchmedia.herokuapp.com/')
+#     print("Called")
+#     print(response.status_code)
+#     print("Waiting for 30 seconds...")
+#     time.sleep(30)
+
+
+response = requests.get('https://vaccinetouchmedia.herokuapp.com/')
+print(response.status_code)
